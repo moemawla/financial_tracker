dc08adc9d4445c8a38484a5ea722365f216e59ab
moemawla <moemawla@hotmail.com>
2021-11-05 12:19:14 +1100

    add schema validations and handle errors

--- financial_tracker/schemas/transaction_schema.py
+++ financial_tracker/schemas/transaction_schema.py
@@ -1,9 +1,11 @@
 from main import ma 
 from models.transactions import Transaction
 from marshmallow_sqlalchemy import auto_field
+from marshmallow.validate import Length
 
 class TransactionSchema(ma.SQLAlchemyAutoSchema):
     transaction_id = auto_field(dump_only=True)
+    transaction_name = auto_field(required=True, validate=Length(min=1))
     
     class Meta:
         model = Transaction
