f4a89620371fddc28216f664517f63bf651b5bbb
moemawla <moemawla@hotmail.com>
2022-03-10 16:15:10 +1100

    add gunicorn and nginx setups

--- install.sh
+++ install.sh
@@ -12,19 +12,29 @@ then
     fi
 fi
 
-# check if pip is installed
-version="$(pip3 -V 2>&1)"
-if [[ $version != "pip"* ]]
-then
-    echo 'Error: You dont have pip installed' >&2
-    exit 1
-fi
+# install pip3 and nginx
+sudo apt update
+sudo apt install -y python3-pip
+sudo apt install -y nginx
 
 # get the absolute path to the directory containing this script
 dir_path=$(dirname $(realpath $0))
 
 # install requirements
-pip3 install -r $dir_path/requirements.txt
+sudo pip3 install -r $dir_path/requirements.txt
+
+
+# setup gunicorn process
+sudo cp $dir_path/financial_tracker_app.service /etc/systemd/system/financial_tracker_app.service
+sudo systemctl daemon-reload
+sudo systemctl start financial_tracker_app
+sudo systemctl enable financial_tracker_app
+
+# setup nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo cp $dir_path/nginx.config /etc/nginx/sites-available/default
+sudo systemctl restart nginx
 
 # drop database tables
 cd financial_tracker/
